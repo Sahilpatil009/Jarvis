143aff0f9069b7694cf583039cd2d019ed4a610f
Sahil Patil <sahilpatilsp009@gmail.com>
2025-03-22 01:42:49 +0530

    Update musicLibrary.py

--- musicLibrary.py
+++ musicLibrary.py
@@ -1,5 +1,8 @@
 music = {
-    "believer": "https://www.youtube.com/watch?v=7wtfhZwyrcc&pp=ygUNYmVsaWV2ZXIgc29uZw%3D%3D",
-    "Perfect": "https://www.youtube.com/watch?v=2Vv-BfVoq4g&pp=ygUMcGVyZmVjdCBzb25n",
-    "Skyfall": "https://www.youtube.com/watch?v=DeumyOzKqgI&pp=ygUMU2t5ZmFsbCBzb25n"
+    "skyfall": "https://www.youtube.com/watch?v=DeumyOzKqgI",
+    "starboy": "https://www.youtube.com/watch?v=plnfIj7dkJE",
+    "perfect": "https://www.youtube.com/watch?v=2Vv-BfVoq4g"
 }
+
+
+
